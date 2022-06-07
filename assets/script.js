86921a62b0bc623c32b5447f2ca6fc0f46f22241
Lucas Saaler <lucassaaler00@gmail.com>
2022-06-07 11:33:00 -0400

    Added comments to code and fixed an issue in the credits of README.md as well as a minor formatting error in the date display

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,22 @@
+// Static selectors
 var hours = [9,10,11,12,13,14,15,16,17]
 var currentDay = $('#currentDay')
 var textAreaEl = $('textarea')
-var currentHour = moment().hour()
 var saveBtn = $('.saveBtn')
 var deleteBtn = $('.delete')
+// System clock
+var currentHour = moment().hour()
+// Boolean on confirm status, defined in function lower
 var confirmOrDeny
+// Array of locally stored inputs
 var local = [localStorage.getItem("local9"), localStorage.getItem("local10"), localStorage.getItem("local11"), localStorage.getItem("local12"), localStorage.getItem("local13"), localStorage.getItem("local14"), localStorage.getItem("local15"), localStorage.getItem("local16"), localStorage.getItem("local17")]
 
+// Runs when page is loaded
 function initialize(){
-    currentDay.text(moment().format('dddd, MMMM do'))
+    // Displays the current date and day of week
+    currentDay.text(moment().format('dddd, MMMM Do'))
     for (let index = 0; index < hours.length; index++) {
+        // Colors past/present/future based on system clock
         if (currentHour > hours[index]){
             $('#' + hours[index]).addClass('past')
         }else if (currentHour == hours[index]){
@@ -17,10 +24,12 @@ function initialize(){
         }else if (currentHour < hours[index]){
             $('#' + hours[index]).addClass('future')
         }
+        // Populates textboxes with any locally stored inputs
         $('#' + hours[index]).text(local[index])
     }
 }
 
+// Saves all inputs
 function saveInput(){
     localStorage.setItem("local9", $('#9').val())
     localStorage.setItem("local10", $('#10').val())
@@ -33,6 +42,7 @@ function saveInput(){
     localStorage.setItem("local17", $('#17').val())
 }
 
+// Clears text from textboxes and sets local storage values to empty
 function decimation(){
     confirmOrDeny = confirm("Are you sure you want to clear your schedule?")
     if (confirmOrDeny){
@@ -43,7 +53,9 @@ function decimation(){
     }    
 }
 
+// Calls initialize() when the page is loaded
 initialize()
 
+// Event listeners for buttons to call functions
 saveBtn.on("click", saveInput)
 deleteBtn.on("click", decimation)
