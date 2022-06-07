a833f93b32642e111ce27a18ab8a913664ddff30
Lucas Saaler <lucassaaler00@gmail.com>
2022-06-07 11:06:45 -0400

    added local storage functionality for save button and population of text-boxes. Also added a clear function, so the user may clear their text boxes and saved events with a click of a button

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,10 @@ var hours = [9,10,11,12,13,14,15,16,17]
 var currentDay = $('#currentDay')
 var textAreaEl = $('textarea')
 var currentHour = moment().hour()
+var saveBtn = $('.saveBtn')
+var deleteBtn = $('.delete')
+var confirmOrDeny
+var local = [localStorage.getItem("local9"), localStorage.getItem("local10"), localStorage.getItem("local11"), localStorage.getItem("local12"), localStorage.getItem("local13"), localStorage.getItem("local14"), localStorage.getItem("local15"), localStorage.getItem("local16"), localStorage.getItem("local17")]
 
 function initialize(){
     currentDay.text(moment().format('dddd, MMMM do'))
@@ -13,7 +17,33 @@ function initialize(){
         }else{
             $('#' + hours[index]).addClass('future')
         }
+        $('#' + hours[index]).text(local[index])
     }
 }
 
+function saveInput(){
+    localStorage.setItem("local9", $('#9').val())
+    localStorage.setItem("local10", $('#10').val())
+    localStorage.setItem("local11", $('#11').val())
+    localStorage.setItem("local12", $('#12').val())
+    localStorage.setItem("local13", $('#13').val())
+    localStorage.setItem("local14", $('#14').val())
+    localStorage.setItem("local15", $('#15').val())
+    localStorage.setItem("local16", $('#16').val())
+    localStorage.setItem("local17", $('#17').val())
+}
+
+function decimation(){
+    confirmOrDeny = confirm("Are you sure you want to clear your schedule?")
+    if (confirmOrDeny){
+        for (let index = 0; index < hours.length; index++) {
+            $('#' + hours[index]).text("")
+            saveInput()
+        }
+    }    
+}
+
 initialize()
+
+saveBtn.on("click", saveInput)
+deleteBtn.on("click", decimation)
